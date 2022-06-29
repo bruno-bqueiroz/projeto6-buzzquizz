7b98d5d492422f6e60fc3efcec1cb9b3491d6e1f
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-29 09:13:13 -0400

    topo

---
+++ script.js
@@ -0,0 +1,16 @@
+let topo;
+let caixa1;
+function corpo(){
+    topo = document.querySelector("div");
+    topo.innerHTML += `
+        <div class="topo">
+            <b>BuzzQuizz</b>
+        </div>
+                     
+    `
+}
+corpo();
+
+function quizz (){
+    
+}
