1f98a76036cd80a95c4d4090173f1bffcbd522bf
victorbdr <victor.ono.badaro@gmail.com>
2022-06-30 13:26:49 -0300

    page 3 rendering api

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let container;
 let caixa = [];
-let dadosQuizz=[];
+let dadosQuizz = [];
 const urlAPI = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
-function corpo(){
-    container = document.querySelector("div");
-    container.innerHTML = `
+function corpo() {
+  container = document.querySelector("div");
+  container.innerHTML = `
         <div class="topo">
             <b>BuzzQuizz</b>
         </div>
                      
-    `
+    `;
 }
 corpo();
 
-function quizz (){
-    container.innerHTML += `
+function quizz() {
+  container.innerHTML += `
         <div class ="quizzes">
             <div class ="quizzUsuario">
             </div>
@@ -28,40 +28,67 @@ function quizz (){
         </div>
 
     `;
-    renderizarMensagens();
+  renderizarMensagens();
 }
 quizz();
 
-
-function buscarDados(){
-    for (let i = 1; i < 9; i++){
-    let promessa = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${i}`);
+function buscarDados() {
+  for (let i = 1; i < 9; i++) {
+    let promessa = axios.get(
+      `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${i}`
+    );
     promessa.then(popularDados);
-    }
+  }
 }
 buscarDados();
 
-
-
-function popularDados(resposta){
-    dadosQuizz = resposta?.data;  
-    renderizarMensagens();
-    
+function popularDados(resposta) {
+  dadosQuizz = resposta?.data;
+  renderizarMensagens();
 }
 popularDados();
 
 function renderizarMensagens() {
-    if (dadosQuizz.length <= 0) return null;
-    let containerMensagens =  document.querySelector(".quizzServidor");
-       
-        containerMensagens.innerHTML += `
+  if (dadosQuizz.length <= 0) return null;
+  let containerMensagens = document.querySelector(".quizzServidor");
+
+  containerMensagens.innerHTML += `
         <div class="quizz1" onclick="Tela2(this)">    
             <img src="${dadosQuizz.image}">
             <strong>${dadosQuizz.title}</strong>
         </div>
     `;
-    console.log(dadosQuizz);
-
+  console.log(dadosQuizz);
 }
 
 renderizarMensagens();
+
+// obter um unico quizz
+function apenasUmQuizz() {
+  const umQuizz = axios.get(
+    "https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes/1"
+  );
+  umQuizz.then(openQuizz);
+  umQuizz.catch(errorQuizz);
+}
+apenasUmQuizz();
+
+function openQuizz(quizz) {
+  console.log(quizz);
+  let quizzQuestions = quizz.data.questions;
+  let quizzOrganize = document.querySelector(".conteudo");
+  quizzOrganize.innerHTML = "";
+  quizzOrganize.innerHTML += `<div class= "quizzHead"><p class="tituloQuizz">${quizz.data.title}</p> <img class="fundo" src="${quizz.data.image}"/> </div>`;
+  quizzQuestions.map((question) => {
+    quizzOrganize.innerHTML += `<div class = "questoes">${question.title} </div>
+        <div class="cor">${question.color}</div>`;
+    question.answers.map((answer) => {
+      quizzOrganize.innerHTML += `<div class="fotos"><img src="${answer.image}"></div>
+            <div class="respostas">${answer.text}</div>`;
+    });
+  });
+}
+
+function errorQuizz() {
+  console.log("nao foi");
+}
