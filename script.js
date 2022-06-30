bd8e4de2b88823bb18c760fed1132e80cd5a67a7
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-30 17:02:57 -0400

    botão_de_criar_novo_Quiz

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let container;
 let caixa = [];
+let dadosQuizzUsuario = [];
 let dadosQuizz = [];
 const urlAPI = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
@@ -28,12 +29,13 @@ function quizz() {
         </div>
 
     `;
+    renderizarMensagensUsuario();
   renderizarMensagens();
 }
 quizz();
 
 function buscarDados() {
-  for (let i = 1; i < 9; i++) {
+  for (let i = 1; i < 10; i++) {
     let promessa = axios.get(
       `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${i}`
     );
@@ -48,6 +50,8 @@ function popularDados(resposta) {
 }
 popularDados();
 
+
+// quizzes do servidor
 function renderizarMensagens() {
   if (dadosQuizz.length <= 0) return null;
   let containerMensagens = document.querySelector(".quizzServidor");
@@ -60,5 +64,32 @@ function renderizarMensagens() {
     `;
   console.log(dadosQuizz);
 }
-
 renderizarMensagens();
+
+
+// quizzes do usuário
+function renderizarMensagensUsuario() {
+    if (dadosQuizzUsuario.length <= 0) {
+        let containerMensagensUsuario = document.querySelector(".quizzUsuario");
+        containerMensagensUsuario.innerHTML = "";
+        containerMensagensUsuario.innerHTML = `
+          <div class="vazio">    
+            <p>Você não criou  nenhum quizz ainda :(</p>
+              <button onclick="criarQuizz(this)"> Criar Quizz </button>
+          </div>
+      `;
+      return;
+
+    }
+    let containerMensagensUsuario = document.querySelector(".quizzUsuario");
+  
+    containerMensagensUsuario.innerHTML += `
+          <div class="quizz1" onclick="Tela2(this)">    
+              <img src="${dadosQuizzUsuario.image}">
+              <strong>${dadosQuizzUsuario.title}</strong>
+          </div>
+      `;
+    console.log(dadosQuizzUsuario);
+  }
+  
+  renderizarMensagensUsuario();
