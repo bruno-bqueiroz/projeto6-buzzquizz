fae60db725f66274752d93ae2e44b572231cc271
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-07-04 08:07:07 -0400

    finalizando tela 3

--- script.js
+++ script.js
@@ -113,11 +113,18 @@ function renderizarMensagensUsuario() {
  // terceira tela. criar as perguntas e respostas
   function tela32(){
     let titulo = document.querySelector("input.titulo").value;
-    let url= document.querySelector("input.url").value;
+    let url;
+    try {
+      let urls = new URL(document.querySelector("input.url").value);
+      url = document.querySelector("input.url").value;
+      console.log("Valid URL!");
+    } catch (err) {
+      console.log("Invalid URL!");
+    }
     let perguntas= document.querySelector("input.nPerguntas").value;
     let niveis = document.querySelector("input.nNiveis").value;
 
-    if(true){//20 < titulo.length < 65 && perguntas > 2  && niveis > 1
+    if(/*20 < titulo.length < 65 && perguntas > 2  && niveis > 1 && url*/true){
       let Elemento = document.querySelector(".tela3");
       Elemento.innerHTML = "";
         Elemento.innerHTML = `
@@ -143,16 +150,84 @@ function renderizarMensagensUsuario() {
               <input placeholder="Resposta incorreta 3" class="Respostaincorreta3"></input>
               <input placeholder="URL da imagem 3" class="URLdaimagem3"></input>
           </div>
-          <button onclick="tela33(this)"> Prosseguir para criar níveis </button>
-         
         </div>
+        <div class="caixa-pergunta">
+          <div class="caixa-pergunta2">
+            <p>Pergunta 2</p>
+            <img src="./imagens/create-icon-26.jpg" onclick="tela322(this)">
+          </div>
+        </div>
+        <div class="caixa-pergunta1">
+          <div class="caixa-pergunta2">
+           <p>Pergunta 3</p>
+           <img src="./imagens/create-icon-26.jpg" onclick="tela323(this)">
+          </div>
+        </div>
+        <button onclick="tela33(this)"> Prosseguir para criar níveis </button>
         `;
 
-    }// else return alert("preencher os dados corretamente");
+    } else return alert("preencher os dados corretamente");
 
   }
   tela32();
+  function tela322(){
+    let Elemento = document.querySelector(".caixa-pergunta");
+      Elemento.innerHTML = `
+      <div class="tela32">
 
+      <div class="pergunta">
+        <div><b>Pergunta 2</b></div>
+          <input placeholder="Texto da pergunta" class="Textodapergunta"></input>
+          <input placeholder="Cor de fundo da pergunta (URL)" class="Cordefundodapergunta"></input>
+      </div>
+      <div class="pergunta">
+        <div><b>Resposta correta</b></div>
+          <input placeholder="Resposta correta" class="Respostacorreta"></input>
+          <input placeholder="URL da imagem" class="URLdaimagem"></input>
+      </div>
+      <div class="pergunta">
+        <div><b>Respostas incorretas</b></div>
+          <input placeholder="Resposta incorreta 1" class="Respostaincorreta1"></input>
+          <input placeholder="URL da imagem 1" class="URLdaimagem1"></input>
+          <input placeholder="Resposta incorreta 2" class="Respostaincorreta2"></input>
+          <input placeholder="URL da imagem 2" class="URLdaimagem2"></input>
+          <input placeholder="Resposta incorreta 3" class="Respostaincorreta3"></input>
+          <input placeholder="URL da imagem 3" class="URLdaimagem3"></input>
+      </div>
+    </div>
+    `;
+
+  }
+  function tela323(){
+
+    let Elemento = document.querySelector(".caixa-pergunta1");
+      Elemento.innerHTML = `
+      <div class="tela32">
+
+      <div class="pergunta">
+        <div><b>Pergunta 3</b></div>
+          <input placeholder="Texto da pergunta" class="Textodapergunta"></input>
+          <input placeholder="Cor de fundo da pergunta (URL)" class="Cordefundodapergunta"></input>
+      </div>
+      <div class="pergunta">
+        <div><b>Resposta correta</b></div>
+          <input placeholder="Resposta correta" class="Respostacorreta"></input>
+          <input placeholder="URL da imagem" class="URLdaimagem"></input>
+      </div>
+      <div class="pergunta">
+        <div><b>Respostas incorretas</b></div>
+          <input placeholder="Resposta incorreta 1" class="Respostaincorreta1"></input>
+          <input placeholder="URL da imagem 1" class="URLdaimagem1"></input>
+          <input placeholder="Resposta incorreta 2" class="Respostaincorreta2"></input>
+          <input placeholder="URL da imagem 2" class="URLdaimagem2"></input>
+          <input placeholder="Resposta incorreta 3" class="Respostaincorreta3"></input>
+          <input placeholder="URL da imagem 3" class="URLdaimagem3"></input>
+      </div>
+    </div>
+    `;
+
+
+  }
  // terceira tela. criar os niveis do quizz
   function tela33(){
     let Textodapergunta = document.querySelector("input.Textodapergunta").value;
@@ -165,8 +240,9 @@ function renderizarMensagensUsuario() {
     let URLdaimagem2 = document.querySelector("input.URLdaimagem2").value;
     let Respostaincorreta3 = document.querySelector("input.Respostaincorreta3").value;
     let URLdaimagem3 = document.querySelector("input.URLdaimagem3").value;
-    
-    let Elemento = document.querySelector(".quizzes");
+
+    if(true){     
+    let Elemento = document.querySelector(".tela3");
    
         Elemento.innerHTML = `
         <b>Agora, decida os níveis!</b>
@@ -175,23 +251,30 @@ function renderizarMensagensUsuario() {
           <div class="pergunta">
             <div><b>Nível 1</b></div>
               <input placeholder="Título do nível" class="Títulodonível"></input>
-              <input placeholder="% de acerto mínima" class="acertomínima"></input>
-              <input placeholder="URL da imagem do nível" class="imagemdonível"></input>
-              <input placeholder="Descrição do nível" class="Descriçãodonível"></input>
+              <input placeholder="% de acerto mínima" class="acertominima"></input>
+              <input placeholder="URL da imagem do nível" class="imagemdonivel"></input>
+              <input placeholder="Descrição do nível" class="descriçãodonivel"></input>
           </div>
+          <div class="caixa-pergunta">
+          <p>Nível 2</p>
+          <img src="./imagens/create-icon-26.jpg" onclick="nivel2(this)">
+        </div>
             <button onclick="tela34(this)"> Finalizar Quizz </button>
-
         </div>
         `;
-    }
+
+    } else return alert("preencher os dados corretamente");
+
+  }
     tela33();
 
     function tela34(){
-      let Títulodonível = document.querySelector("input.Títulodonível").value;
-      let acertomínima = document.querySelector("input.acertomínima").value;
+      let titulodonível = document.querySelector("input.Títulodonível").value;
+      let acertominima = document.querySelector("input.acertomínima").value;
       let imagemdonível = document.querySelector("input.imagemdonível").value;
-      let Descriçãodonível = document.querySelector("input.Descriçãodonível").value;
+      let descriçãodonivel = document.querySelector("input.Descriçãodonível").value;
       
+      if(titulodonível.length > 10  && 0 > acertominima <101   && descriçãodonivel.length > 30 && acertominima == 0 || acertominima == 0){ 
       let Elemento = document.querySelector(".quizzes");
 
       Elemento.innerHTML = `
@@ -207,6 +290,8 @@ function renderizarMensagensUsuario() {
         </div>
         
         `;
+      } else return alert("preencher os dados corretamente");
+
     }
     tela34();
     // retorna a pagina inicial
